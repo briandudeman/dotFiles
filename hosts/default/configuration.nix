8f141cc4bcfb69bafbba4eb37ba7ecf714f0e146
briandudeman <brianjl1944@gmail.com>
2026-04-13 16:08:49 -0400

    131         2026-04-13 16:08:47  26.05.20260204.00c21e4  6.12.68  Unknown                 []              True

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -89,7 +89,7 @@
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    firefox
+    #firefox
   ];
 
   stylix.enable = true;
