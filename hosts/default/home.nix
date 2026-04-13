2383c1227b7ace1a102799b39b38c7893face43d
briandudeman <brianjl1944@gmail.com>
2026-04-13 15:36:14 -0400

    128         2026-04-13 09:34:44  26.05.20260204.00c21e4  6.12.68  Unknown                 []              True

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -22,17 +22,18 @@
     profiles = {
       a_tree = {
         extensions = {
-        packages = with pkgs.nur.repos.rycee.firefox-addons; [
-          ublock-origin
-        ];
-        settings."uBlock0@raymondhill.net".settings = {
-          selectedFilterLists = [
-            "ublock-filters"
-            "ublock-badware"
-            "ublock-privacy"
-            "ublock-unbreak"
-            "ublock-quick-fixes"
+          packages = with pkgs.nur.repos.rycee.firefox-addons; [
+            ublock-origin
           ];
+          settings."uBlock0@raymondhill.net".settings = {
+            selectedFilterLists = [
+              "ublock-filters"
+              "ublock-badware"
+              "ublock-privacy"
+              "ublock-unbreak"
+              "ublock-quick-fixes"
+            ];
+          };
         };
       };
     };
