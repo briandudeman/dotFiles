5e29cf1a78ce210c17490049ace9e2a71f65b101
briandudeman <brianjl1944@gmail.com>
2026-04-23 16:02:28 -0400

    152         2026-04-23 16:02:24  26.05.20260204.00c21e4  6.12.68  Unknown                 []              True

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -111,7 +111,7 @@
           "${modifier}+Return" = "exec ${terminal}";
           "${modifier}+q" = "kill";
           "${modifier}+f" = "exec firefox";  
-          "--release Super_L" = "exec kitty --hold 'echo thing'";
+          "--release Super_L" = "exec kitty --hold ls";
           #"--release Super_L" = "exec 'swaymsg [app_id=kitty] focus || exec wofi --show drun'";  
       };
       
