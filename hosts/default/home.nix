f95247d4a4f6cd1e348b7a30a0c0e449de9073b0
briandudeman <brianjl1944@gmail.com>
2026-04-23 15:39:12 -0400

    148         2026-04-23 15:39:08  26.05.20260204.00c21e4  6.12.68  Unknown                 []              True

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -111,7 +111,7 @@
           "${modifier}+Return" = "exec ${terminal}";
           "${modifier}+q" = "kill";
           "${modifier}+f" = "exec firefox";  
-          "--release Super_L" = "$swaymsg [app_id='wofi'] focus || exec wofi --show drun";  
+          "--release Super_L" = "swaymsg [app_id='kitty'] focus || exec wofi --show drun";  
       };
       
     };
