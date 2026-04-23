717a0208b451ffa536de4f1f22d5a303e16496c9
briandudeman <brianjl1944@gmail.com>
2026-04-23 16:42:23 -0400

    158         2026-04-23 16:42:19  26.05.20260204.00c21e4  6.12.68  Unknown                 []              True

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -111,7 +111,7 @@
           "${modifier}+Return" = "exec ${terminal}";
           "${modifier}+q" = "kill";
           "${modifier}+f" = "exec firefox";  
-          "--release Super_L" = "exec bash '/etc/nixos/menu_toggle.sh'";
+          "--release Super_L" = "pkill wofi || exec wofi --show drun";
           #"--release Super_L" = "exec 'swaymsg [app_id=kitty] focus || exec wofi --show drun'";  
       };
       
