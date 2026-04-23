0e14d39df93a8ff70377665475e89345bb72b2e5
briandudeman <brianjl1944@gmail.com>
2026-04-23 15:15:00 -0400

    142         2026-04-23 14:45:00  26.05.20260204.00c21e4  6.12.68  Unknown                 []              True

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -111,7 +111,7 @@
           "${modifier}+Return" = "exec ${terminal}";
           "${modifier}+q" = "kill";
           "${modifier}+f" = "exec firefox";  
-          "--release Super_L" = "exec wofi --show drun";  
+          "--release Mod4" = "exec wofi --show drun";  
       };
       
     };
